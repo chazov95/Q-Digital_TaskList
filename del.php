38c8cb05898aa6601f8fb5fa59265dc01e61eed5
Alexander Chazov <chazov.a95@gmail.com>
2020-10-16 10:19:35 +0400

    check for autorization

--- del.php
+++ del.php
@@ -1,9 +1,18 @@
 <?php
+session_start();
 require_once('Controllers/taskController.php');
 
 if ($_SESSION['id'] === $_GET['userId']) {
-    $taskId = intval($_GET['taskId']);
 
-    $link->query("DELETE FROM tasks WHERE id = '$taskId'");
+    $userId = intval($_GET['userId']);
+    $query = $link->query("select * from tasks where user_id = '$userId'");
+
+    while ($taskId = $query->fetch_assoc()) {
+        if ($_GET['taskId'] === $taskId['id']) {
+            $taskId = intval($_GET['taskId']);
+
+            $link->query("delete from tasks where id = '$taskId'");
+        };
+    }
 }
 header('Location: /tasklist.php');
--- status.php
+++ status.php
@@ -7,8 +7,14 @@ $connector = new Connector();
 $link = $connector->connect();
 
 if ($_SESSION['id'] === $_GET['userId']) {
-    $taskId = intval($_GET['taskId']);
 
-    $link->query("UPDATE tasks  SET status = 'done' WHERE id = '$taskId'");
+    $userId = intval($_GET['userId']);
+    $query = $link->query("select * from tasks where user_id = '$userId'");
+    while ($taskId = $query->fetch_assoc()) {
+        if ($_GET['taskId'] === $taskId['id']) {
+            $taskId = intval($_GET['taskId']);
+            $link->query("UPDATE tasks  SET status = 'done' WHERE id = '$taskId'");
+        }
+    }
 }
 header('Location: /tasklist.php');
